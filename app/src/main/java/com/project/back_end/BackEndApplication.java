104da8d4ddcbbebd889014e77087d7efe1c9cf94
Daniel <danieleardila11@gmail.com>
2026-05-27 21:39:55 -0400

    add spring modulith dependencies add @Modulithic to setup shared folders

--- app/src/main/java/com/project/back_end/BackEndApplication.java
+++ app/src/main/java/com/project/back_end/BackEndApplication.java
@@ -4,8 +4,13 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.modulith.Modulithic;
 
 @SpringBootApplication
+@Modulithic(
+		systemName = "CMS Backend",
+		sharedModules = {"shared", "enums", "exceptions"}
+)
 public class BackEndApplication {
 
 	public static void main(String[] args) {
